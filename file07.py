27cde929fe9746189c16981ea105e710a83c1873
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-01-17 11:59:36 +0500

    f07

--- file07.py
+++ file07.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    ans = 0
+    for i in data:
+        i.split('\n')
+        if i.isdigit():
+            ans += int(i)
+    return ans
     
 # Read data from file
+f = open('txt_file/data07.txt')
+data = f.read()
+print(main(data))
