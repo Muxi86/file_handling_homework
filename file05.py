69e189470cb5779c38eaeaf14accea5666494a30
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-01-17 11:48:26 +0500

    f05

--- file05.py
+++ file05.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    rows = data.split('\n',1)
+    ans = []
+    for i in rows:
+        ans.append(len(i))
+    return ans
     
 # Read data from file
+f = open('txt_file/data05.txt')
+data = f.read()
+print(main(data))
