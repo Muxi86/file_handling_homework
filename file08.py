3c60b476e7560a8aa2d63d077d0730fd2a033736
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-01-17 12:15:15 +0500

    f08

--- file08.py
+++ file08.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    maxi = 0
+    for i in data:
+        if i.isdigit():
+            maxi = i
+            if maxi < i:
+                maxi = i
+    return maxi
 
 # Read data from file
+f = open('txt_file/data08.txt')
+data = f.read()
+print(main(data))
